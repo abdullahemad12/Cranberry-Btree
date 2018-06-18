3fbd7e226ad2e6b26579fcffc885f6e6781b51a9
Abdullah Emad <abdullahem1997@hotmail.com>
2018-06-18 11:17:14 +0200

    Improve the bt_delete_helper test

--- testsuite.c
+++ testsuite.c
@@ -1115,14 +1115,24 @@ void split_full_root_test(void)
 
 void bt_delete_helper_test(void)
 {
-	int n = 104;
-	int keys[] = {
-		 58, 67, 100, 56, 84, 99, 44, 22, 10, 85, 83, 61, 71, 45, 89, 81, 97, 34, 41, 98, 74, 37, 2, 40, 82, 79, 69, 36, 30, 20, 53, 60, 65, 21, 14, 19, 88, 59, 32, 96, 46, 28, 64, 93, 52, 91, 80, 48, 8, 47, 77, 43, 27, 86, 66, 63, 17, 62, 70, 73, 18, 7, 15, 55, 51, 16, 75, 13, 72, 1, 6, 31, 3, 78, 57, 90, 87, 25, 94, 92, 24, 9, 39, 42, 4, 5, 76, 50, 35, 29, 49, 38, 11, 54, 95, 33, 26, 12, 23, 400,300, 150, 450, 454
-	};
-	btree_t* bt = bt_create(10);
+	int n = 15;
+	int m = 1000;
+	int keys[n];
+	int dup[m]; /*to catch duplicate values*/
+	memset(keys, 0, n);
+	memset(dup, 0, m);
+	
+	
+	btree_t* bt = bt_create(3);
 	srand(time(NULL));   
 
-
+	for(int i = 0; i < n; i++)
+	{
+		int r = rand() % m;
+		dup[r]++;
+		keys[i] = r;
+	}
+	
 	
 	for(int i = 0; i < n; i++)
 	{
@@ -1140,11 +1150,16 @@ void bt_delete_helper_test(void)
 		}
 		CU_ASSERT_PTR_NOT_NULL_FATAL(bt_search(bt, keys[i]));
 		bt_delete_helper(bt, NULL, bt->root, keys[i]);
-		if(bt_search(bt, keys[i]) != NULL)
+		dup[keys[i]]--;
+		/*assert that all instances were removed*/
+		if(dup[keys[i]] == 0)
 		{
-			printf("dup: %d\n", keys[i]);
+			if(bt_search(bt, keys[i]) != NULL)
+			{
+				printf("dup: %d\n", keys[i]);
+			}
+			CU_ASSERT_PTR_NULL_FATAL(bt_search(bt, keys[i]));
 		}
-		CU_ASSERT_PTR_NULL_FATAL(bt_search(bt, keys[i]));
 	}
 	bt_destroy(bt, free);
 }
